af7945f7f3fe1d776884d371c9cb38a215613e3f
Fafemi Adeola <adeola5678@gmail.com>
2022-03-16 20:55:40 +0100

    updates

--- contracts/stake.sol
+++ contracts/stake.sol
@@ -3,11 +3,10 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract StakeContract {
     IERC721 public _BoredApeNFT;
-    IERC20Metadata public _BoredApeToken;
   struct  stakers {
         uint amount;
         uint timeStaked;
@@ -23,7 +22,7 @@ contract StakeContract {
         _BoredApeNFT = IERC721(0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D);
     }
 
-    function Stake (uint _amount, uint _timeStaked, IERC20Metadata token  ) public returns (bool) {
+    function Stake (uint _amount, uint _timeStaked, IERC20 token  ) public returns (bool) {
          require(_amount > 0, "You need to stake at least some tokens");
         uint256 tokenBalance = token.balanceOf(msg.sender);
         require(tokenBalance>= _amount, "You do not have enough tokens");
@@ -42,7 +41,7 @@ contract StakeContract {
     }
 
 
-      function WithDrawStake ( uint _presentTime, IERC20Metadata token ) public returns (bool) {
+      function WithDrawStake ( uint _presentTime, IERC20 token ) public returns (bool) {
         require (records[msg.sender].amount > 0, " You need to stake to withdraw");
         stakers memory user= records[msg.sender];
         if(_presentTime >= user.timeDue){
